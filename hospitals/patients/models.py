e04a0f9622e57390150002701e1309fe598a31b1
Joanne Jo <joannehjo@gmail.com>
2021-02-19 21:48:47 -0500

    push2

--- hospitals/patients/models.py
+++ hospitals/patients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Patients(models.Model):
+    firstname = models.CharField(max_length=100, null=True)
+    lastphone = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    checkin_time = models.DateTimeField(auto_now_add=True)
--- hospitals/patients/urls.py
+++ hospitals/patients/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('queue/', views.patientqueue),
-    path('administration/', views.administration),
+    path('', views.home, name='home'),
+    path('queue/', views.patientqueue, name='queue'),
+    path('administration/', views.administration, name='administration'),
 ]
